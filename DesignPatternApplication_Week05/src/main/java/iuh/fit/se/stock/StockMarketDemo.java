b4bbe464ad653dc4df21ab30c7efc60f0210e774
Lan Anh <dhlananh2309@gmail.com>
2025-04-20 12:45:49 +0700

    fix: correct stock symbol from GOOGL to GOOGLE in StockMarketDemo

--- DesignPatternApplication_Week05/src/main/java/iuh/fit/se/stock/StockMarketDemo.java
+++ DesignPatternApplication_Week05/src/main/java/iuh/fit/se/stock/StockMarketDemo.java
@@ -2,16 +2,15 @@ package iuh.fit.se.stock;
 
 public class StockMarketDemo {
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		// Tạo cổ phiếu
-        Stock googleStock = new Stock("GOOGL", 2500.0);
+        Stock googleStock = new Stock("GOOGLE", 2500.0);
 
         // Tạo các nhà đầu tư (Observers), tự động đăng ký theo dõi cổ phiếu
         Investor investor1 = new Investor("Nhà đầu tư A", googleStock);
         Investor investor2 = new Investor("Nhà đầu tư B", googleStock);
 
         // Thay đổi giá cổ phiếu
-        System.out.println("Giá cổ phiếu GOOGL hiện tại: " + googleStock.getPrice());
+        System.out.println("Giá cổ phiếu GOOGLE hiện tại: " + googleStock.getPrice());
         googleStock.setPrice(2550.0);
         System.out.println("---");
         googleStock.setPrice(2600.0);
